20d600930c08a79a12c48fdfc702b7b0f03f9c91
dmytro <dvinokurov777@gmail.com>
2016-05-24 13:27:08 +0300

    added forwarding to article servlet

--- src/main/java/ua/goit/web/servlet/ArticleServlet.java
+++ src/main/java/ua/goit/web/servlet/ArticleServlet.java
@@ -1,5 +1,7 @@
 package ua.goit.web.servlet;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,5 +13,13 @@ public class ArticleServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String topic = req.getParameter("topic");
         System.out.println(topic);
+        forward(req, resp);
     }
+
+    private void forward(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher("/page2.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
 }
